cecb148e415354f0f7a873eb518ed0257bcf9b00
GarvinH <garvinh2001@hotmail.com>
2020-08-30 17:21:12 -0400

    Add popover to portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Jumbotron, Container, Row} from "react-bootstrap";
+import { Jumbotron, Container, Row } from "react-bootstrap";
 import Project from "./Project/Project";
-import memo from "../../assets/memo-io.png";
-import forecast from "../../assets/forecast-io.png";
+import projects from "./Portfolio.json";
 
 const Portfolio = () => (
   <Jumbotron style={{ height: "100%", margin: 0 }}>
     <Container fluid>
       <Row style={{ width: "100%" }}>
-        <Project img={memo} title="Memo.io" />
-        <Project img={forecast} title="Forecast.io" />
+        {projects.map((project) => (
+          <Project
+            img={require(`../../${project.img}`)}
+            title={project.title}
+            content={project.content}
+          />
+        ))}
       </Row>
     </Container>
   </Jumbotron>
--- src/components/Portfolio/Project/Project.js
+++ src/components/Portfolio/Project/Project.js
@@ -1,23 +1,61 @@
-import React from "react";
-import { Popover, OverlayTrigger, Card, Col } from "react-bootstrap";
+import React, { useEffect } from "react";
+import { Popover, OverlayTrigger, Card, Col, Button } from "react-bootstrap";
 
-const Details = (
-  <Popover>
-    <Popover.Content>test</Popover.Content>
-  </Popover>
+const Details = React.forwardRef(
+  ({ popper, content, show: _, ...props }, ref) => {
+    useEffect(() => {
+      popper.scheduleUpdate();
+    }, [content, popper]);
+
+    return (
+      <Popover ref={ref} content {...props}>
+        {content.desc ? <Popover.Title>{content.desc}</Popover.Title> : null}
+        <Popover.Content>
+          <h5>Technology/Libraries:</h5>
+          <ul>
+            {content.tech.map((lib) => (
+              <li>{lib}</li>
+            ))}
+          </ul>
+        </Popover.Content>
+        <Popover.Title>
+          {content.website ? (
+            <p>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href={content.website}
+              >
+                Website
+              </a>
+            </p>
+          ) : null}
+          <a target="_blank" rel="noopener noreferrer" href={content.github}>
+            Github
+          </a>
+        </Popover.Title>
+      </Popover>
+    );
+  }
 );
 
-const Project = ({ img, title }) => (
-  <OverlayTrigger trigger="hover" overlay={Details} placement="bottom">
-    <Col align="center">
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={img} />
-        <Card.Body style={{borderTop: "1px black solid"}}>
-          <Card.Title>{title}</Card.Title>
-        </Card.Body>
-      </Card>
-    </Col>
-  </OverlayTrigger>
+const Project = ({ img, title, content }) => (
+  <Col align="center">
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={img} />
+      <Card.Body style={{ borderTop: "1px black solid" }}>
+        <Card.Title>
+          <OverlayTrigger
+            trigger="click"
+            overlay={<Details content={content} />}
+            placement="bottom"
+          >
+            <Button variant="dark">{title}</Button>
+          </OverlayTrigger>
+        </Card.Title>
+      </Card.Body>
+    </Card>
+  </Col>
 );
 
 export default Project;
