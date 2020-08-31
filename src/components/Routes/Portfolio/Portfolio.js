e20d7ce96a0cc175e900d566cd7fd989779c86d5
GarvinH <garvinh2001@hotmail.com>
2020-08-31 12:17:32 -0400

    Improve mobile feel of portfolio

--- src/components/Routes/Portfolio/Portfolio.js
+++ src/components/Routes/Portfolio/Portfolio.js
@@ -6,7 +6,7 @@ import projects from "./Portfolio.json";
 const Portfolio = () => (
   <Jumbotron>
       <h1 style={{margin: "2rem 0 5rem"}} className="heading">Portfolio</h1>
-      <Row style={{ width: "100%" }}>
+      <Row style={{ width: "100%", margin: 0 }}>
         {projects.map((project) => (
           <Project
             img={require(`../../../${project.img}`)}
