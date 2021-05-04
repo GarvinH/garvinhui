5cf07afe62bd34e603bc3ea5be94e7ff8b6b9914
GarvinH <garvinh2001@hotmail.com>
2021-05-04 19:32:39 -0400

    Remove react router bootstrap; Adjust background color

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,28 +1,18 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => (
   <Navbar fixed="top" bg="dark" variant="dark" expand="lg">
-    <Navbar.Brand href="#home">Garvin Hui</Navbar.Brand>
+    <Navbar.Brand as={NavLink} to="/">Garvin Hui</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        <LinkContainer to="/" exact>
-          <Nav.Link>Home</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/experience">
-          <Nav.Link>Experience</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/portfolio">
-          <Nav.Link>Portfolio</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/education">
-          <Nav.Link>Education</Nav.Link>
-        </LinkContainer>
-        <LinkContainer to="/interests">
-          <Nav.Link>Interests</Nav.Link>
-        </LinkContainer>
+          <Nav.Link as={NavLink} to="/" exact>Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/experience">Experience</Nav.Link>
+          <Nav.Link as={NavLink} to="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link as={NavLink} to="/education">Education</Nav.Link>
+          <Nav.Link as={NavLink} to="/interests">Interests</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
