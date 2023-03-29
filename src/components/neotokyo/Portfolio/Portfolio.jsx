965ea4cbc50734a0bb7fd1ed4eb0939e3c7b7d9d
Garvin Hui <garvinh2001@hotmail.com>
2023-03-29 11:30:54 -0400

    Add extra content to portfolio section neotokyo

--- src/components/neotokyo/Portfolio/Portfolio.jsx
+++ src/components/neotokyo/Portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import Section from "../Section/Section"
 import portfolio from "text/Portfolio.json"
 
-const Project = ({title, content}) => <div>
+const Project = ({ title, content }) => <div>
     <h2>{title}</h2>
     <p dangerouslySetInnerHTML={{ __html: content.tech.join(", ") }} />
     {content.desc && <p>{content.desc}</p>}
@@ -14,8 +14,8 @@ class Portfolio extends React.Component {
             <Section
                 id="portfolio"
                 sectionHeader={portfolio.title}
-                extraContent={[["test"]]}
-                extraContentHeader={["test"]}>
+                extraContent={portfolio.optional.map((project_category) => project_category.projects.map((project) => <Project {...project} />))}
+                extraContentHeader={portfolio.optional.map((project_category) => project_category.heading)}>
                 {portfolio.mandatory.projects.map((project) => <Project {...project} />)}
             </Section>
         </div>
